bcfe79ff8101a984a7466f510ef881fecb701af2
cyril <cyril.zakka@gmail.com>
2019-03-23 21:11:29 +0200

    Updated for MuJoCo 2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(name='gym_assist',
       version='0.0.1',
-      install_requires=['gym>=0.2.3', 
-      					'mujoco_py>=1.50', 
+      install_requires=['gym>=0.12.0', 
+      					'mujoco-py<2.1,>=2.0.2.0a1', 
       					'imageio']
      )  
